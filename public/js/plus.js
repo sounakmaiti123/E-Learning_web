cd650d53c2683423d82e0f36ec753c2a6bc283ef
Sounak Maiti <maitisounak04@gmail.com>
2026-05-20 11:21:57 +0530

    Secured environment variables and updated auth UI

--- public/js/plus.js
+++ public/js/plus.js
@@ -3,26 +3,6 @@
 (function () {
     "use strict";
 
-    /* ---------- GLOBAL SMOOTH SCROLL (LENIS) ---------- */
-    // Note: Make sure to include the Lenis CDN in your HTML file(s)
-    if (typeof Lenis !== 'undefined') {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            direction: 'vertical',
-            gestureDirection: 'vertical',
-            smooth: true,
-            smoothTouch: false,
-            touchMultiplier: 2,
-        });
-
-        function raf(time) {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        }
-        requestAnimationFrame(raf);
-    }
-
     /* ---------- BILLING TOGGLE ---------- */
     const billingToggle = document.getElementById("billingToggle");
     const monthlyLabel = document.getElementById("monthlyLabel");
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,24 @@
 console.log("EduChain UI Loaded");
 
+/* ---------- GLOBAL SMOOTH SCROLL (LENIS) ---------- */
+if (typeof Lenis !== 'undefined') {
+    const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        direction: 'vertical',
+        gestureDirection: 'vertical',
+        smooth: true,
+        smoothTouch: false,
+        touchMultiplier: 2,
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+}
+
 const section = document.querySelector(".learning-content");
 
 if (section) {
